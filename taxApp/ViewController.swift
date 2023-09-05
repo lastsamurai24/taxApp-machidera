537a5c0fbd92088ae940157e9420f1cedf94a075
samurai <lastsamurai24@icloud.com>
2023-09-05 12:02:58 +0900

    部品配置後

--- taxApp/ViewController.swift
+++ taxApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var showLabel: UILabel!
+    var taxArray: [String] = []
+    @IBOutlet weak var addTextField: UITextField!
+    
+    @IBOutlet weak var taxRateSegment: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let userDfaults = UserDefaults.standard
+        if userDfaults.object(forKey: "tax") != nil {
+            taxArray = userDfaults.object(forKey: "tax") as! [String]
+        
+        }
     }
-
-
+    
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        let userDefaults = UserDefaults.standard
+        taxArray = []
+        if addTextField.text != "" {
+            taxArray.append(addTextField.text!)
+            userDefaults.set(taxArray, forKey: "tax")
+            self.navigationController?.popViewController(animated: true)
+            //questionsキーで保存されてるオブジェクトがあったら
+            if userDefaults.object(forKey: "tax")  != nil {
+            }
+            addTextField.text = ""
+        }
+    }
 }
-
