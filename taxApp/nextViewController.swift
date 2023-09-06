b6a1037c537fadf93f0ec00037b17b864dba0e84
samurai <lastsamurai24@icloud.com>
2023-09-06 21:09:52 +0900

    改善しました

--- taxApp/nextViewController.swift
+++ taxApp/nextViewController.swift
@@ -12,34 +12,36 @@ class nextViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        let resultArray = UserDefaults.standard.object(forKey: "item") as! [Double]
-        UserDefaults.standard.set([], forKey: "item")
-
-        let sum = resultArray.reduce(0) { (num1:Double, num2:Double) -> Double in
-            return num1 + num2
+        
+            let resultArray = UserDefaults.standard.object(forKey: "item") as! [Double]
+            UserDefaults.standard.set([0], forKey: "item")
+            
+            let sum = resultArray.reduce(0) { (num1:Double, num2:Double) -> Double in
+                return num1 + num2
+                
+            }
+            
+            resultLabel.text = String(format: "%.0f", sum)
             
         }
-
-        resultLabel.text = String(format: "%.0f", sum)
-
+        
     }
     
-    }
-
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 
